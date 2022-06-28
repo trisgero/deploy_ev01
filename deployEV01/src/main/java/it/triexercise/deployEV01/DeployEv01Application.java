787ce966396672ff78cc24f0ea7662b61ca2bf5e
Tristan Geronimo <tristanpgeronimo@gmail.com>
2022-06-28 13:40:07 +0200

    Add files via upload

---
+++ deployEV01/src/main/java/it/triexercise/deployEV01/DeployEv01Application.java
@@ -0,0 +1,13 @@
+package it.triexercise.deployEV01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployEv01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployEv01Application.class, args);
+	}
+
+}
---
+++ deployEV01/src/test/java/it/triexercise/deployEV01/DeployEv01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.triexercise.deployEV01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployEv01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
